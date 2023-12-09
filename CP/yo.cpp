1dc231da7c977fffa1e54a11e594f5fec94e0691
Arvind Keshri <arvidce@gmail.com>
2023-12-09 17:51:48 +0530

    power of 4

--- CP/yo.cpp
+++ CP/yo.cpp
@@ -3,22 +3,13 @@
 #include <iostream>
 using namespace std;
 
-void table(int n, int current, int range){
-	  if(current>range) return ;
-	  else {
-	  	cout<<n << " X " << current<<"= "<< n*current<<endl;
-	  	table(n, current+1, range);
-	  }
+int helper(int n){
+	return n==0?1:4*helper(n-1);
 }
 
 
 int main() {
-	int n, range;
-	cout<<"Enter number = ";
-	cin>>n;
-	cout<<"Enter range = ";
-	cin>>range;
-    table(n, 0, range);
+	cout<<helper(4);
     
     return 0;
 }
