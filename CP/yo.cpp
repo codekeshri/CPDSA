549c00a62ee0dbf8b3757541e2717f20562bec70
Arvind Keshri <arvidce@gmail.com>
2023-12-09 16:22:41 +0530

    factorial using recursion

--- CP/yo.cpp
+++ CP/yo.cpp
@@ -1,7 +1,14 @@
+//Factorial
+
 #include <iostream>
 using namespace std;
 
-const int N = 0;
+int factorial(int n){
+	return n==1?1:n*factorial(n-1);
+}
+
+
 int main() {
-    
+    cout<<factorial(5);
+    return 0;
 }
